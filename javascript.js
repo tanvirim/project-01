ce78ead3621cf056529669db86260645c6e93632
tanvirim <blackpanther141532@GMAIL.COM>
2022-04-09 15:59:45 +0600

    first commit

---
+++ javascript.js
@@ -0,0 +1,11 @@
+
+const promp = require('prompt-sync')();
+    
+
+let fName = promp("enter 1");
+let lName = promp("enter num 2 ");
+
+let x = parseInt(fName) ;
+let y = parseInt(lName);
+fullName = x + y ;
+console.log("my name is",fullName) ;33
